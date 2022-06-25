c4125f80c0b71b4de2a3ca1ee7018cc9781174e1
mobyw <44370805+mobyw@users.noreply.github.com>
2022-06-26 00:23:25 +0800

    💄 Change logo

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -76,7 +76,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         ],
 
         sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-        logo: '/img/logo.png', // 导航栏logo
+        logo: '/img/swjtu.png', // 导航栏logo
         repo: 'swjtuhub/swjtu-wiki', // 导航栏右侧生成Github链接
         searchMaxSuggestions: 5, // 搜索结果显示最大数
         lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
